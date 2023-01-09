990a10add0fd2677ca6fdb0c0c8c732552b674f0
Igor <igor@devfire.io>
2023-01-08 23:55:21 -0500

    WIP on the buffered reader

--- prime-time/src/main.rs
+++ prime-time/src/main.rs
@@ -1,54 +1,61 @@
+use tokio::io::{AsyncReadExt, AsyncWriteExt, AsyncBufReadExt};
 use tokio::net::TcpListener;
-use tokio::prelude::*;
-use serde::{Deserialize, Serialize};
+use tokio::io::BufReader;
 
-#[derive(Debug, Serialize, Deserialize)]
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use validator::{Validate, ValidationError};
+
+#[derive(Debug, Deserialize, Validate)]
 struct Request {
-    // fields go here
+    #[validate(length(min = 1), custom = "validate_method")]
+    method: String,
+
+    #[validate(range(min = 1))]
+    number: f32,
+    // other fields go here
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+fn validate_method(method: &str) -> Result<(), ValidationError> {
+    if method != "isPrime" {
+        // the value of the username will automatically be added later
+        return Err(ValidationError::new("ERROR: method must be isPrime"));
+    }
+    Ok(())
+}
+
+#[derive(Debug, Serialize)]
 struct Response {
     // fields go here
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:12345".parse()?;
-    let listener = TcpListener::bind(&addr).await?;
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-    let server = async move {
-        let mut incoming = listener.incoming();
-        while let Some(conn) = incoming.next().await {
-            let conn = conn?;
-            tokio::spawn(async move {
-                let mut buf = Vec::new();
-                // read from the connection and append the data to `buf`
-                let n = conn.read_buf(&mut buf).await?;
-                // try to deserialize the request
-                let request: Result<Request, _> = serde_json::from_slice(&buf[..n]);
-                match request {
-                    Ok(request) => {
-                        // request is valid, handle it
-                        println!("received request: {:?}", request);
-                        let response = Response { /* fields go here */ };
-                        let response_bytes = serde_json::to_vec(&response)?;
-                        conn.write_all(&response_bytes).await?;
-                    }
+    loop {
+        let (socket, _) = listener.accept().await?;
+
+        let mut socket = BufReader::new(socket);
+
+        tokio::spawn(async move {
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+
+                let mut line = String::new();
+
+                let n = match socket.read_line(&mut line).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
                     Err(e) => {
-                        // request is invalid, send an error response
-                        println!("received invalid request: {}", e);
-                        let response = Response { /* fields go here */ };
-                        let response_bytes = serde_json::to_vec(&response)?;
-                        conn.write_all(&response_bytes).await?;
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
                     }
-                }
-                Ok(())
-            });
-        }
-    };
-
-    server.await?;
-
-    Ok(())
+                };
+            }
+        });
+    }
 }
