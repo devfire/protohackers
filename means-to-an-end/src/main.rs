4afbcc2975a2a48c6709345b235403ba470cb526
Igor <igor@devfire.io>
2023-01-15 13:57:47 -0500

    ranges adjusted

--- means-to-an-end/src/main.rs
+++ means-to-an-end/src/main.rs
@@ -5,15 +5,15 @@ use tokio::io::{self, AsyncReadExt};
 use tokio::net::TcpListener;
 
 // https://doc.rust-lang.org/std/primitive.i32.html#method.from_be_bytes
-// fn read_be_i32(input: &mut &[u8]) -> i32 {
-//     let (int_bytes, rest) = input.split_at(std::mem::size_of::<i32>());
-//     *input = rest;
-//     i32::from_be_bytes(int_bytes.try_into().expect("Failed in read_be_i32 to convert from slice to array"))
-// }
-
-fn make_array(input: &[u8]) -> [u8;4] {
-    input.try_into().expect("slice with incorrect length")
+fn read_be_i32(input: &mut &[u8]) -> i32 {
+    let (int_bytes, rest) = input.split_at(std::mem::size_of::<i32>());
+    *input = rest;
+    i32::from_be_bytes(int_bytes.try_into().expect("Failed in read_be_i32 to convert from slice to array"))
 }
+
+// fn make_array(input: &[u8]) -> [u8;4] {
+//     input.try_into().expect("slice with incorrect length")
+// }
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -41,11 +41,10 @@ async fn main() -> io::Result<()> {
                 });
 
                 let msg_type = &buffer[0];
-                let first_half_decoded = i32::from_be_bytes(make_array(&buffer[1..4]));
-                let second_half_decoded = i32::from_be_bytes(make_array(&buffer[5..8]));
 
-                // let first_half_decoded = read_be_i32(&mut &buffer[1..4]);
-                // let second_half_decoded = read_be_i32(&mut &buffer[5..8]);
+                // read slices 1,2,3,4
+                let first_half_decoded = read_be_i32(&mut &buffer[1..=4]);
+                let second_half_decoded = read_be_i32(&mut &buffer[5..n]);
 
                 println!("Type: {}, first: {}, second: {}", msg_type, first_half_decoded, second_half_decoded);
             }
