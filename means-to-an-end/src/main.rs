7dad24fa21f8e97bbf563c26c7d9b43a61852fe3
Igor <igor@devfire.io>
2023-01-15 23:19:17 -0500

    with logging and sqlite

--- means-to-an-end/src/main.rs
+++ means-to-an-end/src/main.rs
@@ -1,21 +1,39 @@
 // use std::io;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 // use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, AsyncReadExt};
+// use sqlite;
+use log::info;
 use tokio::io::{self, AsyncReadExt};
 use tokio::net::TcpListener;
-use sqlite::State;
-
-
 
 // https://doc.rust-lang.org/std/primitive.i32.html#method.from_be_bytes
 fn read_be_i32(input: &[u8]) -> i32 {
     i32::from_be_bytes(input[..4].try_into().unwrap())
 }
 
+fn insert_data(timestamp: &i32, price: &i32) {
+    unimplemented!()
+}
+
+fn query_data(time1: &i32, time2: &i32) {
+    unimplemented!()
+}
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 8080);
     let listener = TcpListener::bind(&addr).await?;
+
+    let create_table_query =
+        "CREATE TABLE messages (msg_type INTEGER, begin_time INTEGER, end_time INTEGER)";
+
+    info!("Creating the table.");
+
+    let connection = sqlite::open(":memory:").expect("Failed to open sqlite");
+
+    connection
+        .execute(create_table_query)
+        .expect("Failed to create table");
 
     while let Ok((stream, addr)) = listener.accept().await {
         println!("New connection from {}", addr);
@@ -26,8 +44,11 @@ async fn main() -> io::Result<()> {
 
         tokio::spawn(async move {
             loop {
-                let mut buffer = [0;9];
-                let n = reader.read_exact(&mut buffer).await.expect("Unable to read buffer");
+                let mut buffer = [0; 9];
+                let n = reader
+                    .read_exact(&mut buffer)
+                    .await
+                    .expect("Unable to read buffer");
                 println!("Bytes received: {} buffer length: {}", n, buffer.len());
                 if n == 0 {
                     break;
@@ -37,18 +58,28 @@ async fn main() -> io::Result<()> {
                 //     print!("byte #{}, {:#010b} ",i, &buffer[i]);
                 // });
 
+                // The message format is:
+                // Byte:  |  0  |  1     2     3     4  |  5     6     7     8  |
+                // Type:  |char |         int32         |         int32         |
+                // Value: | 'I' |       timestamp       |         price         |
+                //
+
                 let msg_type = &buffer[0];
 
-                // read slices 1,2,3,4
+                // Read slices 1,2,3,4.
+                // Since we have a slice rather than an array, fallible conversion APIs can be used
                 let first_half_decoded = read_be_i32(&buffer[1..=4]);
                 let second_half_decoded = read_be_i32(&buffer[5..n]);
 
-                println!("Type: {}, first: {}, second: {}", msg_type, first_half_decoded, second_half_decoded);
+                println!(
+                    "Type: {}, first: {}, second: {}",
+                    msg_type, first_half_decoded, second_half_decoded
+                );
 
                 match msg_type {
-                    73 => insert_data(first_half_decoded, second_half_decoded),
-                    81 => query_data(first_half_decoded, second_half_decoded),
-                    _ => println!("Got something weird, ignoring.")
+                    73 => insert_data(&first_half_decoded, &second_half_decoded),
+                    81 => query_data(&first_half_decoded, &second_half_decoded),
+                    _ => println!("Got something weird, ignoring."),
                 }
             }
         });
