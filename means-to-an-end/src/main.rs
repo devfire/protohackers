c56b6a69e9ce2795c4e36042fcbf12da18cf6678
Igor <igor@devfire.io>
2023-01-23 21:45:04 -0500

    removed loop

--- means-to-an-end/src/main.rs
+++ means-to-an-end/src/main.rs
@@ -16,7 +16,7 @@ fn calculate_average(btree_db: &BTreeMap<i32, i32>, start: &i32, end: &i32) -> i
     let mut final_count = 0; // number of entries
 
     println!("Calculating average between {} and {}", start, end);
-    
+
     // https://doc.rust-lang.org/stable/std/collections/struct.BTreeMap.html#method.range
     for (count, (key, value)) in btree_db.range((Included(start), Included(end))).enumerate() {
         println!("{key}: {value}");
@@ -24,6 +24,8 @@ fn calculate_average(btree_db: &BTreeMap<i32, i32>, start: &i32, end: &i32) -> i
         final_count = count;
     }
     // average is total / count
+    println!("Total: {} count: {}", total, final_count);
+
     total / final_count as i32
 }
 
@@ -52,48 +54,48 @@ async fn process(stream: TcpStream) {
     // A hashmap is used to store data
     let mut db: BTreeMap<i32, i32> = BTreeMap::new();
 
-    loop {
-        //declare a buffer precisely 9 bytes long
-        let mut buffer = [0; 9];
+    // loop {
+    //declare a buffer precisely 9 bytes long
+    let mut buffer = [0; 9];
 
-        let n = reader
-            .read_exact(&mut buffer)
-            .await
-            .expect("Unable to read from buffer");
+    let n = reader
+        .read_exact(&mut buffer)
+        .await
+        .expect("Unable to read from buffer");
 
-        if n == 0 {
-            break;
+    // if n == 0 {
+    //     break;
+    // }
+
+    let msg_type = &buffer[0];
+
+    // Read slices 1,2,3,4.
+    // Since we have a slice rather than an array, fallible conversion APIs can be used
+    let first_half_decoded = read_be_i32(&buffer[1..=4]);
+    let second_half_decoded = read_be_i32(&buffer[5..n]);
+
+    println!(
+        "Type: {}, first: {}, second: {}",
+        msg_type, first_half_decoded, second_half_decoded
+    );
+
+    match msg_type {
+        73 => {
+            db.insert(first_half_decoded, second_half_decoded);
         }
+        81 => {
+            let avg = calculate_average(&db, &first_half_decoded, &second_half_decoded);
+            writer
+                .write_all(avg.to_string().as_bytes())
+                .await
+                .expect("Writing avg failed");
 
-        let msg_type = &buffer[0];
-
-        // Read slices 1,2,3,4.
-        // Since we have a slice rather than an array, fallible conversion APIs can be used
-        let first_half_decoded = read_be_i32(&buffer[1..=4]);
-        let second_half_decoded = read_be_i32(&buffer[5..n]);
-
-        println!(
-            "Type: {}, first: {}, second: {}",
-            msg_type, first_half_decoded, second_half_decoded
-        );
-
-        match msg_type {
-            73 => {
-                db.insert(first_half_decoded, second_half_decoded);
-            }
-            81 => {
-                let avg = calculate_average(&db, &first_half_decoded, &second_half_decoded);
-                writer
-                    .write_all(avg.to_string().as_bytes())
-                    .await
-                    .expect("Writing avg failed");
-
-                writer
-                    .write_all(b"\n")
-                    .await
-                    .expect("Failed ending buffer write");
-            }
-            _ => panic!("unknown msg type"),
+            writer
+                .write_all(b"\n")
+                .await
+                .expect("Failed ending buffer write");
         }
+        _ => panic!("unknown msg type"),
+        // }
     }
 }
