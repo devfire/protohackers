2b5c8594b83fb021773b2c07d613a18f3eaa5086
Igor Kantor <igor@devfire.io>
2023-01-23 22:41:25 -0500

    working code, without sqlite, with BTreeMap (#2)
    Working problem 2 implementation.

--- means-to-an-end/src/main.rs
+++ means-to-an-end/src/main.rs
@@ -1,49 +1,107 @@
-// use std::io;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-// use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, AsyncReadExt};
-use tokio::io::{self, AsyncReadExt};
-use tokio::net::TcpListener;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
+
+use std::collections::BTreeMap;
 
 // https://doc.rust-lang.org/std/primitive.i32.html#method.from_be_bytes
 fn read_be_i32(input: &[u8]) -> i32 {
     i32::from_be_bytes(input[..4].try_into().unwrap())
 }
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 8080);
-    let listener = TcpListener::bind(&addr).await?;
-
-    while let Ok((stream, addr)) = listener.accept().await {
-        println!("New connection from {}", addr);
-
-        let stream = stream;
-        let (reader, mut _writer) = tokio::io::split(stream);
-        let mut reader = io::BufReader::new(reader);
-
-        tokio::spawn(async move {
-            loop {
-                let mut buffer = [0;9];
-                let n = reader.read_exact(&mut buffer).await.expect("Unable to read buffer");
-                println!("Bytes received: {} buffer length: {}", n, buffer.len());
-                if n == 0 {
-                    break;
-                }
-
-                // (0..9).for_each(|i| {
-                //     print!("byte #{}, {:#010b} ",i, &buffer[i]);
-                // });
-
-                let msg_type = &buffer[0];
-
-                // read slices 1,2,3,4
-                let first_half_decoded = read_be_i32(&buffer[1..=4]);
-                let second_half_decoded = read_be_i32(&buffer[5..n]);
-
-                println!("Type: {}, first: {}, second: {}", msg_type, first_half_decoded, second_half_decoded);
-            }
-        });
+fn calculate_average(btree_db: &BTreeMap<i32, i32>, start: &i32, end: &i32) -> i32 {
+    if start > end {
+        return 0;
     }
 
-    Ok(())
+    use std::ops::Bound::Included;
+    let mut total: i64 = 0; // sum of prices
+    let mut final_count = 0; // number of entries
+
+    println!("Calculating average between {} and {}", start, end);
+
+    // https://doc.rust-lang.org/stable/std/collections/struct.BTreeMap.html#method.range
+    for (count, (_, value)) in btree_db.range((Included(start), Included(end))).enumerate() {
+        // println!("{key}: {value}");
+
+        // need * otherwise casting fails
+        total += *value as i64;
+        final_count = count;
+    }
+
+    final_count += 1;
+
+    // average is total / count
+    println!("Total: {} count: {}", total, final_count);
+
+    (total / final_count as i64) as i32
+}
+
+#[tokio::main]
+async fn main() {
+    // Bind the listener to the address
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+    loop {
+        // The second item contains the ip and port of the new connection.
+        let (stream, addr) = listener.accept().await.unwrap();
+
+        println!("New connection from {}", addr);
+
+        // A new task is spawned for each inbound socket.  The socket is
+        // moved to the new task and processed there.
+        tokio::spawn(async move {
+            process(stream).await;
+        });
+    }
+}
+
+async fn process(stream: TcpStream) {
+    let (mut reader, mut writer) = tokio::io::split(stream);
+
+    // A hashmap is used to store data
+    let mut db: BTreeMap<i32, i32> = BTreeMap::new();
+
+    loop {
+        //declare a buffer precisely 9 bytes long
+        let mut buffer = [0; 9];
+
+        let n = reader
+            .read_exact(&mut buffer)
+            .await
+            .expect("Unable to read from buffer");
+
+        if n == 0 {
+            break;
+        }
+
+        let msg_type = &buffer[0];
+
+        // Read slices 1,2,3,4.
+        // Since we have a slice rather than an array, fallible conversion APIs can be used
+        let first_half_decoded = read_be_i32(&buffer[1..=4]);
+        let second_half_decoded = read_be_i32(&buffer[5..n]);
+
+        // println!(
+        //     "Type: {}, first: {}, second: {}",
+        //     msg_type, first_half_decoded, second_half_decoded
+        // );
+
+        match msg_type {
+            73 => {
+                db.insert(first_half_decoded, second_half_decoded);
+            }
+            81 => {
+                let avg = calculate_average(&db, &first_half_decoded, &second_half_decoded);
+                println!("Sending average {} back", avg);
+
+                writer
+                    .write_all(&avg.to_be_bytes())
+                    .await
+                    .expect("Writing avg failed");
+            }
+            _ => panic!("unknown msg type"),
+        }
+    } //end of loop
 }
