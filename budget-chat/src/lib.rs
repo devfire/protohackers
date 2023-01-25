41677ffaca111b90430bc69e548b19504df50c29
Igor Kantor <igor@devfire.io>
2023-01-25 16:58:03 -0500

    Budget chat (#4)
    * init budget chat commit
    
    * working get_username()
    
    * working logger (#3)
    
    * init tokio based implementation of chat
    
    * files renamed
    
    * added lib.rs
    
    * everything without present list usernames
    
    * initial hashmap of nested structs
    
    * first try with usernames
    
    * pub async fn get_everyone
    
    * removed unneeded asyncs
    
    * clean lib.rs
    
    * clean build
    
    * switched to send to sender
    
    * removed reference, added invalid username c heck
    
    * validating usernames
    
    * fixed valid_username fn
    
    * cleaning up, final working chat

---
+++ budget-chat/src/lib.rs
@@ -0,0 +1,114 @@
+use std::collections::HashMap;
+use std::io;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::net::TcpStream;
+use tokio::sync::{mpsc, Mutex};
+use tokio_util::codec::{Framed, LinesCodec};
+
+/// Shorthand for the transmit half of the message channel.
+pub type Tx = mpsc::UnboundedSender<String>;
+
+/// Shorthand for the receive half of the message channel.
+pub type Rx = mpsc::UnboundedReceiver<String>;
+
+#[derive(Debug)]
+pub struct UserDetails {
+    username: String,
+    tx: Tx,
+}
+/// Data that is shared between all peers in the chat server.
+///
+/// This is the set of `Tx` handles for all connected clients. Whenever a
+/// message is received from a client, it is broadcasted to all peers by
+/// iterating over the `peers` entries and sending a copy of the message on each
+/// `Tx`.
+pub struct Shared {
+    pub peers: HashMap<SocketAddr, UserDetails>,
+}
+
+/// The state for each connected client.
+pub struct Peer {
+    /// The TCP socket wrapped with the `Lines` codec, defined below.
+    ///
+    /// This handles sending and receiving data on the socket. When using
+    /// `Lines`, we can work at the line level instead of having to manage the
+    /// raw byte operations.
+    pub lines: Framed<TcpStream, LinesCodec>,
+
+    /// Receive half of the message channel.
+    ///
+    /// This is used to receive messages from peers. When a message is received
+    /// off of this `Rx`, it will be written to the socket.
+    pub rx: Rx,
+}
+
+// The user might expect to be able to use Default as the type can be constructed without arguments.
+// https://rust-lang.github.io/rust-clippy/master/index.html#new_without_default
+impl Default for Shared {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Shared {
+    /// Create a new, empty, instance of `Shared`.
+    pub fn new() -> Self {
+        Shared {
+            peers: HashMap::new(),
+        }
+    }
+
+    /// Send a `LineCodec` encoded message to every peer, except
+    /// for the sender.
+    pub async fn broadcast(&mut self, sender: SocketAddr, message: &str) {
+        for peer in self.peers.iter_mut() {
+            if *peer.0 != sender {
+                let _ = peer.1.tx.send(message.into());
+            }
+        }
+    }
+
+    pub fn send_to_sender(&mut self, sender: SocketAddr, message: &str) {
+        for peer in self.peers.iter_mut() {
+            if *peer.0 == sender {
+                let _ = peer.1.tx.send(message.into());
+            }
+        }
+    }
+
+    pub fn get_everyone(&mut self, sender: SocketAddr) -> Vec<String> {
+        let mut everyone = vec![];
+        for peer in self.peers.iter() {
+            if *peer.0 != sender {
+                everyone.push(peer.1.username.clone());
+            }
+        }
+        everyone
+    }
+    
+}
+
+impl Peer {
+    /// Create a new instance of `Peer`.
+    pub async fn new(
+        state: Arc<Mutex<Shared>>,
+        lines: Framed<TcpStream, LinesCodec>,
+        username: String,
+    ) -> io::Result<Peer> {
+        // Get the client socket address
+        let addr = lines.get_ref().peer_addr()?;
+
+        // Create a channel for this peer
+        let (tx, rx) = mpsc::unbounded_channel();
+
+        // Add an entry for this `Peer` in the shared state map.
+        state
+            .lock()
+            .await
+            .peers
+            .insert(addr, UserDetails { username, tx });
+
+        Ok(Peer { lines, rx })
+    }
+}
---
+++ budget-chat/src/main.rs
@@ -0,0 +1,156 @@
+// heavily borrowed from https://raw.githubusercontent.com/tokio-rs/tokio/master/examples/chat.rs
+#![warn(rust_2018_idioms)]
+
+use futures::SinkExt;
+use std::net::SocketAddr;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
+use tokio_stream::StreamExt;
+use tokio_util::codec::{Framed, LinesCodec};
+
+use std::{error::Error, sync::Arc};
+
+use budget_chat::{Peer, Shared};
+
+use env_logger::Env;
+use log::{error, info};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Setup the logging framework
+    let env = Env::default()
+        .filter_or("LOG_LEVEL", "info")
+        .write_style_or("LOG_STYLE", "always");
+
+    env_logger::init_from_env(env);
+
+    // Create the shared state. This is how all the peers communicate.
+    //
+    // The server task will hold a handle to this. For every new client, the
+    // `state` handle is cloned and passed into the task that processes the
+    // client connection.
+    let state = Arc::new(Mutex::new(Shared::new()));
+
+    info!("Starting the chat server.");
+
+    // Bind the listener to the address
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+    info!("Server is running.");
+
+    loop {
+        // Asynchronously wait for an inbound TcpStream.
+        let (stream, addr) = listener.accept().await?;
+
+        // Clone a handle to the `Shared` state for the new connection.
+        let state = Arc::clone(&state);
+
+        // Spawn our handler to be run asynchronously.
+        tokio::spawn(async move {
+            info!("accepted connection from {}", addr);
+            if let Err(e) = process(state, stream, addr).await {
+                info!("an error occurred; error = {:?}", e);
+            }
+        });
+    }
+}
+
+// make sure the username is at least 1 char,
+// and consists of alphanumeric chars only.
+// Return FALSE if username is empty OR not alphanumeric.
+fn valid_username(username: &String) -> bool {
+    !username.is_empty() && username.chars().all(char::is_alphanumeric)
+}
+
+/// Process an individual chat client
+async fn process(
+    state: Arc<Mutex<Shared>>,
+    stream: TcpStream,
+    addr: SocketAddr,
+) -> Result<(), Box<dyn Error>> {
+    let mut lines = Framed::new(stream, LinesCodec::new());
+
+    // Send a prompt to the client to enter their username.
+    lines
+        .send("Welcome to budgetchat! What shall I call you? ")
+        .await?;
+
+    // Read the first line from the `LineCodec` stream to get the username.
+    let username = match lines.next().await {
+        Some(Ok(line)) => line,
+        // We didn't get a line so we return early here.
+        _ => {
+            error!("Failed to get username from {}. Client disconnected.", addr);
+            return Ok(());
+        }
+    };
+
+    if !valid_username(&username) {
+        error!("Invalid username: {}", username);
+        return Ok(());
+    }
+
+    // Register our peer with state which internally sets up some channels.
+    let mut peer = Peer::new(state.clone(), lines, username.clone()).await?;
+
+    // A client has connected, let's let everyone know.
+    {
+        let mut state = state.lock().await;
+        let msg = format!("* {} has entered the room.", username);
+        info!("{}", msg);
+        state.broadcast(addr, &msg).await;
+    }
+
+    // Publish all present users' names
+    {
+        let mut state = state.lock().await;
+        let everyone_else = state.get_everyone(addr);
+
+        let msg = format!("* The room contains: {:?}", everyone_else);
+        info!("{}", msg);
+        state.send_to_sender(addr, &msg);
+    }
+
+    // Process incoming messages until our stream is exhausted by a disconnect.
+    loop {
+        tokio::select! {
+            // A message was received from a peer. Send it to the current user.
+            Some(msg) = peer.rx.recv() => {
+                peer.lines.send(&msg).await?;
+            }
+            result = peer.lines.next() => match result {
+                // A message was received from the current user, we should
+                // broadcast this message to the other users.
+                Some(Ok(msg)) => {
+                    let mut state = state.lock().await;
+                    let msg = format!("[{}] {}", username, msg);
+                    info!("{}", msg);
+                    state.broadcast(addr, &msg).await;
+                }
+                // An error occurred.
+                Some(Err(e)) => {
+                    error!(
+                        "an error occurred while processing messages for {}; error = {:?}",
+                        username,
+                        e
+                    );
+                }
+                // The stream has been exhausted.
+                None => break,
+            },
+        }
+    }
+
+    // If this section is reached it means that the client was disconnected!
+    // Let's let everyone still connected know about it.
+    {
+        let mut state = state.lock().await;
+        state.peers.remove(&addr);
+
+        let msg = format!("* {} has left the room", username);
+        info!("{}", msg);
+        state.broadcast(addr, &msg).await;
+    }
+
+    Ok(())
+}
