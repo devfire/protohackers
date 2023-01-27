07e24213d9b42516787cac65103f0ed8945027bf
Igor <igor@devfire.io>
2023-01-27 14:56:10 -0500

    initial db implementation

--- unusual-database/src/main.rs
+++ unusual-database/src/main.rs
@@ -1,28 +1,32 @@
-//! An UDP echo server that just sends back everything that it receives.
-//!
-//! If you're on Unix you can test this out by in one terminal executing:
-//!
-//!     cargo run --example echo-udp
-//!
-//! and in another terminal you can run:
-//!
-//!     cargo run --example connect -- --udp 127.0.0.1:8080
-//!
-//! Each line you type in to the `nc` terminal should be echo'd back to you!
-
 #![warn(rust_2018_idioms)]
 
-use std::error::Error;
+use anyhow::{Context, Ok, Result};
+
 use std::net::SocketAddr;
 use std::{env, io};
 use tokio::net::UdpSocket;
 
-/// There are only two types of requests: insert and retrieve. 
-/// Insert allows a client to insert a value for a key, 
+
+use log::{error, info};
+
+/// There are only two types of requests: insert and retrieve.
+/// Insert allows a client to insert a value for a key,
 /// and retrieve allows a client to retrieve the value for a key.
 enum MessageType {
     Insert,
     Retrieve,
+}
+
+fn get_message_type(msg: &[u8]) -> MessageType {
+    for ch in msg.iter() {
+        if char::from(*ch) == '=' {
+            info!("Received an insert message");
+            return MessageType::Insert
+        }
+    }
+    info!("Received an retrieve message");
+    
+    MessageType::Retrieve
 }
 
 struct Server {
@@ -44,9 +48,12 @@ impl Server {
             // If so then we try to send it back to the original source, waiting
             // until it's writable and we're able to do so.
             if let Some((size, peer)) = to_send {
-                let amt = socket.send_to(&buf[..size], &peer).await?;
+                // let amt = socket.send_to(&buf[..size], &peer).await?;
+                
+                info!("Received {:?} from {}", buf, peer);
+                let message_type = get_message_type(&buf);
 
-                println!("Echoed {}/{} bytes to {}", amt, size, peer);
+                // println!("Echoed {}/{} bytes to {}", amt, size, peer);
             }
 
             // If we're here then `to_send` is `None`, so we take a look for the
@@ -57,7 +64,7 @@ impl Server {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<()> {
     let addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string());
