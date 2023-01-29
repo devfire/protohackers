35a9e8383c701743635cf822ce4a2dbf375b1d36
Igor <igor@devfire.io>
2023-01-28 19:15:57 -0500

    added hashmap and many functions

--- unusual-database/src/main.rs
+++ unusual-database/src/main.rs
@@ -1,6 +1,8 @@
 #![warn(rust_2018_idioms)]
 
+use std::collections::HashMap;
 use std::error::Error;
+use std::hash::Hash;
 use std::net::SocketAddr;
 use std::{env, io};
 use tokio::net::UdpSocket;
@@ -12,25 +14,24 @@ struct Server {
     socket: UdpSocket,
     buf: Vec<u8>,
     received: Option<(usize, SocketAddr)>,
+    db: HashMap<String, String>,
 }
 
 /// There are only two types of requests: insert and retrieve.
 /// Insert allows a client to insert a value for a key,
 /// and retrieve allows a client to retrieve the value for a key.
-enum MessageType {
-    Insert,
-    Retrieve,
-}
+// enum MessageType {
+//     Insert,
+//     Retrieve,
+// }
 
-fn get_message_type(msg: &[u8]) -> MessageType {
-    for ch in msg.iter() {
-        if char::from(*ch) == '=' {
-            print!("{}",ch);
-            return MessageType::Insert;
-        }
-    }
-
-    MessageType::Retrieve
+/// This function returns the key & value pair
+/// if this is an INSERT op, or None if this is a RETRIEVE op
+/// https://doc.rust-lang.org/std/primitive.str.html#method.split_once
+fn get_kv_pair(msg: &[u8]) -> Option<(String, String)> {
+    String::from_utf8_lossy(msg)
+        .split_once('=')
+        .map(|(key, value)| -> (String, String) { (key.to_string(), value.to_string()) })
 }
 
 impl Server {
@@ -39,24 +40,26 @@ impl Server {
             socket,
             mut buf,
             mut received,
+            mut db,
         } = self;
 
         loop {
             // First we check to see if there's a message we need to process.
-            // If so then we try to send it back to the original source, waiting
-            // until it's writable and we're able to do so.
             if let Some((size, peer)) = received {
-                info!("Received {:#?} from {}", &buf[..size], peer);
-                match get_message_type(&buf[..size]) {
-                    MessageType::Insert => info!("Received an insert message"),
-                    MessageType::Retrieve => info!("Received a retrieve message"),
+                let msg = &buf[..size];
+                info!("Received a msg from {}", peer);
+
+                // If this comes back with something, that means this was an Insert,
+                // so we just add it to hashmap
+                if let Some((k, v)) = get_kv_pair(msg) {
+                    info!("Insert message type detected, adding.");
+                    db.insert(k, v);
+                } else {
+                    info!("Retreive message type detected, replying.");
                 }
             }
 
-            /*
-            If we're here then `received` is `None`, so we take a look for the
-            next message we're going to process.
-            */
+            // If we're here then `received` is `None`, so we look for the next message to process.
             received = Some(socket.recv_from(&mut buf).await?);
         }
     }
@@ -82,6 +85,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         socket,
         buf: vec![0; 1024],
         received: None,
+        db: HashMap::new(),
     };
 
     // This starts the server task.
