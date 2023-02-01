21b48a609f7c45ae50cf12c3691de906d5879380
Igor <igor@devfire.io>
2023-01-31 23:28:02 -0500

    re writing with regex inplace replacement

--- mob-in-the-middle/src/main.rs
+++ mob-in-the-middle/src/main.rs
@@ -1,13 +1,9 @@
-use std::net::SocketAddr;
-
-use regex::Regex;
-
 use env_logger::Env;
 use log::{error, info};
 
 use tokio::{
+    io::{copy, AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
-    io::copy,
 };
 
 use anyhow::Result;
@@ -26,55 +22,58 @@ async fn main() -> Result<()> {
     let listener = TcpListener::bind("0.0.0.0:8080").await?;
     info!("Ready to steal crypto!");
 
-    loop {
-        // Asynchronously wait for an inbound TcpStream.
-        let (client_facing_stream, addr) = listener.accept().await?;
-
-        // Spawn our handler to be run asynchronously.
-        tokio::spawn(async move {
-            info!("accepted connection from {}", addr);
-            if let Err(e) = process(client_facing_stream, addr).await {
-                error!("an error occurred; error = {:?}", e);
-            }
-        });
+    // Accept incoming connections
+    while let Ok((client, _)) = listener.accept().await {
+        // Spawn a task to handle each client
+        tokio::spawn(process(client, "chat.protohackers.com:16963"));
     }
-}
-
-async fn process(to_client_stream: TcpStream, addr: SocketAddr) -> Result<()> {
-    info!(
-        "Establishing a connection to the upstream server on behalf of {}.",
-        addr
-    );
-    let to_server_stream = TcpStream::connect("chat.protohackers.com:16963").await?;
-    info!("Connection established.");
-
-    let (mut server_reader, mut server_writer) = tokio::io::split(to_server_stream);
-    // let mut server_reader = io::BufReader::new(server_reader);
-
-    let (mut client_reader, mut client_writer) = tokio::io::split(to_client_stream);
-    // let mut client_reader = io::BufReader::new(client_reader);
-
-    let client_to_server = copy(&mut client_reader, &mut server_writer);
-    let server_to_client = copy(&mut server_reader, &mut client_writer);
-
-    let (_,_) = tokio::join!(client_to_server, server_to_client);
 
     Ok(())
 }
 
-// fn replace_substring(s: &str, old: &str, new: &str) -> String {
-//     s.replace(old, new)
-// }
+/// Defines a new asynchronous function `process` that takes two arguments:
+/// `client`, a mutable reference to a TcpStream, and `server_addr`, a string slice of the remote server.
+async fn process(client_stream: TcpStream, server_addr: &str) -> Result<()> {
+    info!(
+        "Establishing a connection to the upstream server on behalf of {}.",
+        server_addr
+    );
+    let server_stream = TcpStream::connect("chat.protohackers.com:16963").await?;
+    info!("Connection established.");
 
-// fn get_substring(s: &str) -> Option<&str> {
-//     let re = Regex::new(r"^ 7[a-zA-Z0-9]{25,34} $").unwrap();
-//     re.find(s).map(|m| m.as_str())
-// }
+    let (mut server_reader, mut server_writer) = tokio::io::split(server_stream);
+    // let mut server_reader = io::BufReader::new(server_reader);
 
-// fn steal_crypto(line: &str) -> String {
-//     if let Some(boguscoin_address) = get_substring(line) {
-//         replace_substring(line, boguscoin_address, "7YWHMfk9JZe0LM0g1ZauHuiSxhI")
-//     } else {
-//         String::from(line)
-//     }
-// }
+    let (mut client_reader, mut client_writer) = tokio::io::split(client_stream);
+    // let mut client_reader = io::BufReader::new(client_reader);
+
+    let client_to_server = async move {
+        let mut buf = [0; 1024];
+
+        loop {
+            let n = match client_reader.read(&mut buf).await {
+                Ok(n) => n,
+                Err(e) => {
+                    error!("Error forwarding data from client: {}", e);
+                    break;
+                }
+            };
+            if n == 0 {
+                break;
+            }
+
+            let data = String::from_utf8(buf[..n].to_vec())
+                .unwrap()
+                .replace(r"^ 7[a-zA-Z0-9]{25,34} $", "7YWHMfk9JZe0LM0g1ZauHuiSxhI")
+                .into_bytes();
+
+            server_writer.write_all(&data).await.unwrap();
+        }
+    };
+
+    let server_to_client = copy(&mut server_reader, &mut client_writer);
+
+    let (_, _) = tokio::join!(client_to_server, server_to_client);
+
+    Ok(())
+}
