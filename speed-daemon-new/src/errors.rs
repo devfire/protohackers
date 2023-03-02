f0f73eb4c07043f07f0b64b340b77701d0b0b66d
Igor <igor@devfire.io>
2023-03-02 10:29:00 -0500

    added the errors.rs

---
+++ speed-daemon-new/src/errors.rs
@@ -0,0 +1,11 @@
+use thiserror::Error;
+
+/// SpeedDaemonError enumerates all possible errors returned by this library.
+#[derive(Error, Debug)]
+pub enum SpeedDaemonError {
+    /// When the client does something that this protocol specification declares "an error", 
+    /// the server must send the client an appropriate Error message,
+    /// and immediately disconnect that client.
+    #[error("Invalid message type")]
+    InvalidMessage,
+}
