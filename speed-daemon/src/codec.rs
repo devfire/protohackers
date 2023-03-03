d6aecacd46d0198f320981abf1e4a54e376be813
Igor <igor@devfire.io>
2023-03-03 12:21:24 -0500

    added impl codec + moved directory

---
+++ speed-daemon/src/codec.rs
@@ -0,0 +1,21 @@
+use tokio_util::codec::{Decoder, Encoder};
+
+use bytes::{Buf, BufMut, BytesMut};
+use std::{cmp, fmt, io, str, usize};
+
+#[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct MessageCodec {}
+
+impl MessageCodec {
+    /// Creates a new [`MessageCodec`].
+    pub fn new() -> Self { Self {  } }
+
+    
+}
+
+impl Default for MessageCodec {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
