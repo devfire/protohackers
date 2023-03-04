4b823979f4644a0928661bdf6f5093f3768c403e
Igor <igor@devfire.io>
2023-03-03 23:18:58 -0500

    parser cleanup

--- speed-daemon/src/parsers.rs
+++ speed-daemon/src/parsers.rs
@@ -1,11 +1,9 @@
 use nom::{
     branch::alt,
     bytes::streaming::{tag, take},
-    error::{Error, ErrorKind},
-    multi::{length_count, length_data, count},
+    multi::length_count,
     number::streaming::{be_u16, be_u32, be_u8, u8},
-    sequence::delimited,
-    Err, IResult,
+    IResult,
 };
 
 use crate::message::MessageType;
@@ -22,10 +20,7 @@ fn parse_plate(input: &[u8]) -> nom::IResult<&[u8], MessageType> {
 
     // Convert the bytes to a String
     let plate = String::from_utf8(string_bytes.to_vec()).map_err(|_| {
-        nom::Err::Failure(nom::error::Error::new(
-            input,
-            nom::error::ErrorKind::Char,
-        ))
+        nom::Err::Failure(nom::error::Error::new(input, nom::error::ErrorKind::Char))
     })?;
 
     // timestamp: u32
@@ -35,30 +30,29 @@ fn parse_plate(input: &[u8]) -> nom::IResult<&[u8], MessageType> {
     Ok((input, MessageType::Plate { plate, timestamp }))
 }
 
-pub fn parse_want_heartbeat (input: &[u8]) -> IResult<&[u8], MessageType> {
+pub fn parse_want_heartbeat(input: &[u8]) -> IResult<&[u8], MessageType> {
     //0x40: WantHeartbeat (Client->Server)
     let (input, _) = tag([0x40])(input)?;
     let (input, interval) = be_u32(input)?;
-    Ok((input, MessageType::WantHeartbeat{interval}))
+    Ok((input, MessageType::WantHeartbeat { interval }))
 }
 
-pub fn parse_i_am_camera (input: &[u8]) -> IResult<&[u8], MessageType> {
+pub fn parse_i_am_camera(input: &[u8]) -> IResult<&[u8], MessageType> {
     //0x80: IAmCamera (Client->Server)
     let (input, _) = tag([0x80])(input)?;
     let (input, road) = be_u16(input)?;
     let (input, mile) = be_u16(input)?;
     let (input, limit) = be_u16(input)?;
-    Ok((input, MessageType::IAmCamera { road, mile, limit}))
+    Ok((input, MessageType::IAmCamera { road, mile, limit }))
 }
 
-pub fn parse_i_am_dispatcher (input: &[u8]) -> IResult<&[u8], MessageType> {
+pub fn parse_i_am_dispatcher(input: &[u8]) -> IResult<&[u8], MessageType> {
     // 0x81: IAmDispatcher (Client->Server)
     let (input, _) = tag([0x81])(input)?;
-    let (input, numroads)  = be_u8(input)?;
+    let (input, numroads) = be_u8(input)?;
     let (input, roads) = length_count(u8, be_u16)(input)?;
     Ok((input, MessageType::IAmDispatcher { numroads, roads }))
 }
-
 
 pub fn parse_message(input: &[u8]) -> IResult<&[u8], MessageType> {
     let (input, message) = alt((
