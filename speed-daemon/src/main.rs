64107ba92fccdf9cc65284c9c0a19ebbb37cefda
Igor <igor@devfire.io>
2023-03-07 12:29:50 -0500

    implemented tx clone

--- speed-daemon/src/main.rs
+++ speed-daemon/src/main.rs
@@ -14,6 +14,8 @@ use futures::TryStreamExt;
 
 use tokio::time::{sleep, Duration};
 
+use tokio::sync::mpsc;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // Setup the logging framework
@@ -26,11 +28,6 @@ async fn main() -> anyhow::Result<()> {
     info!("Starting the speed daemon server.");
 
     // Create the shared state. This is how all the peers communicate.
-    //
-    // The server task will hold a handle to this. For every new client, the
-    // `state` handle is cloned and passed into the task that processes the
-    // client connection.
-    // let state = Arc::new(tokio::sync::Mutex::new(Shared::new()));
 
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 8080);
 
@@ -67,6 +64,13 @@ async fn process(stream: TcpStream, addr: SocketAddr) -> anyhow::Result<()> {
     // NOTE: split is not necessary here since Framed does both read & write.
     let mut framed: Framed<TcpStream, MessageCodec> = Framed::new(stream, MessageCodec {});
 
+    // Create a new channel with a capacity of at most 32.
+    // The mpsc channel is used to send commands to the task managing the client connection.
+    // The multi-producer capability allows messages to be sent from many tasks.
+    // Creating the channel returns two values, a sender and a receiver.
+    // The two handles are used separately. They may be moved to different tasks.
+    let (tx, mut rx) = mpsc::channel::<OutboundMessageType>(32);
+
     while let Some(message) = framed.try_next().await? {
         info!("From {}: {:?}", addr, message);
 
@@ -96,12 +100,10 @@ async fn process(stream: TcpStream, addr: SocketAddr) -> anyhow::Result<()> {
                     "Client {} requested heartbeat every {} deciseconds.",
                     addr, interval
                 );
-
+                let heartbeat_tx = tx.clone();
                 // Spawn our handler to be run asynchronously.
                 tokio::spawn(async move {
-                    loop {
-                        sleep(Duration::from_secs(interval as u64)).await;
-                    }
+                    handle_want_hearbeat(heartbeat_tx, interval).await;
                 });
             }
 
@@ -126,8 +128,10 @@ fn handle_ticket(message: InboundMessageType) {
     todo!()
 }
 #[allow(unused)]
-fn handle_want_hearbeat(interval: u32) {
-    todo!()
+async fn handle_want_hearbeat(tx: tokio::sync::mpsc::Sender<OutboundMessageType>, interval: u32) {
+    loop {
+        sleep(Duration::from_secs(interval as u64)).await;
+    }
 }
 #[allow(unused)]
 fn handle_i_am_camera(message: InboundMessageType) {
