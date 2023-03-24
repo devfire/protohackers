08430279ea53ae6daf9d9759b4d8f501eec9d351
Igor <igor@devfire.io>
2023-03-24 15:48:02 -0400

    added .round()

--- speed-daemon/src/state.rs
+++ speed-daemon/src/state.rs
@@ -163,7 +163,9 @@ impl Db {
                 // need to x3600 to convert mi/sec to mi/hr. Later, we'll x100 the actual ticket to comply with the spec.
                 let distance_traveled = camera_mile1.abs_diff(camera_mile2) as u32;
                 let time_traveled = p_ts_pair1.timestamp.abs_diff(p_ts_pair2.timestamp);
-                let average_speed = (distance_traveled * 3600) / time_traveled;
+                let average_speed =
+                    (((distance_traveled * 3600) / time_traveled) as f32).round() as u32; // we must round here
+                    
                 info!(
                     "For plate {} road {} distance {} time traveled {}",
                     plate, road1, distance_traveled, time_traveled
@@ -297,7 +299,8 @@ impl Db {
                     // need to x3600 to convert mi/sec to mi/hr. Later, we'll x100 the actual ticket to comply with the spec.
                     let distance_traveled = camera_mile1.abs_diff(camera_mile2) as u32;
                     let time_traveled = p_ts_pair1.timestamp.abs_diff(p_ts_pair2.timestamp);
-                    let average_speed = (distance_traveled * 3600) / time_traveled;
+                    let average_speed =
+                        (((distance_traveled * 3600) / time_traveled) as f32).round() as u32; // we must round here
                     info!(
                         "For plate {} road {} distance {} time traveled {}",
                         plate, road1, distance_traveled, time_traveled
