1dc668d35a4d043050be6898deb5e7500700aefe
Igor <igor@devfire.io>
2023-03-25 18:08:21 -0400

    fix heartbeat at yellow

--- speed-daemon/src/handlers/heartbeat.rs
+++ speed-daemon/src/handlers/heartbeat.rs
@@ -1,31 +1,31 @@
-use log::{error, info};
+use log::error;
 // use speed_daemon::errors::SpeedDaemonError;
 use speed_daemon::message::OutboundMessageType;
 use tokio::sync::mpsc;
-// use tokio::time;
-use tokio::time::sleep;
+use tokio::time;
+// use tokio::time::sleep;
 use tokio::time::Duration;
 
-pub async fn handle_want_hearbeat(interval: u32, tx: mpsc::Sender<OutboundMessageType>) {
+pub async fn handle_want_hearbeat(
+    interval: u32,
+    tx: mpsc::Sender<OutboundMessageType>,
+) -> anyhow::Result<()> {
     let interval = interval as f32 / 10.0;
-    // let mut tick_interval = time::interval(Duration::from_secs_f32(interval));
-    info!("Setting tick interval to {}", interval);
-    loop {
-        let tx = tx.clone();
-        tokio::task::spawn_blocking(move || {
-            // match tx.send(OutboundMessageType::Heartbeat) {
-            //     Ok(_) => {}
-            //     Err(e) => {
-            //         error!("Unable to send heartbeat, client {} disconnected", e);
-            //         return;
-            //     }
-            // }
+    tokio::spawn(async move {
+        loop {
+            match tx.send(OutboundMessageType::Heartbeat).await {
+                Ok(_) => {}
+                Err(e) => {
+                    error!("Unable to send heartbeat, client {} disconnected", e);
+                    return;
+                }
+            }
 
-            tx.blocking_send(OutboundMessageType::Heartbeat)
-                .expect("Unable to send a heartbeat.");
+            // sleep(Duration::from_millis(interval as u64)).await;
+            let mut tick_interval = time::interval(Duration::from_secs_f32(interval));
+            tick_interval.tick().await;
+        }
+    });
 
-            // tick_interval.tick();
-        });
-        sleep(Duration::from_secs_f32(interval)).await;
-    }
+    Ok(())
 }
