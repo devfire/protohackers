290a6d339d7659dc82ddd385bbb9c6aea4bd0441
Igor <igor@devfire.io>
2023-03-25 18:00:24 -0400

    moved await outside spawn block

--- speed-daemon/src/handlers/heartbeat.rs
+++ speed-daemon/src/handlers/heartbeat.rs
@@ -2,19 +2,17 @@ use log::{error, info};
 // use speed_daemon::errors::SpeedDaemonError;
 use speed_daemon::message::OutboundMessageType;
 use tokio::sync::mpsc;
-use tokio::time;
+// use tokio::time;
 use tokio::time::sleep;
 use tokio::time::Duration;
 
-pub async fn handle_want_hearbeat(
-    interval: u32,
-    tx: mpsc::Sender<OutboundMessageType>,
-) -> anyhow::Result<()> {
+pub async fn handle_want_hearbeat(interval: u32, tx: mpsc::Sender<OutboundMessageType>) {
     let interval = interval as f32 / 10.0;
     // let mut tick_interval = time::interval(Duration::from_secs_f32(interval));
     info!("Setting tick interval to {}", interval);
-    tokio::task::spawn_blocking(move || {
-        loop {
+    loop {
+        let tx = tx.clone();
+        tokio::task::spawn_blocking(move || {
             // match tx.send(OutboundMessageType::Heartbeat) {
             //     Ok(_) => {}
             //     Err(e) => {
@@ -23,13 +21,11 @@ pub async fn handle_want_hearbeat(
             //     }
             // }
 
-            sleep(Duration::from_secs_f32(interval));
-            
-            tx.blocking_send(OutboundMessageType::Heartbeat).expect("Unable to send a heartbeat.");
+            tx.blocking_send(OutboundMessageType::Heartbeat)
+                .expect("Unable to send a heartbeat.");
 
             // tick_interval.tick();
-        }
-    });
-
-    Ok(())
+        });
+        sleep(Duration::from_secs_f32(interval)).await;
+    }
 }
