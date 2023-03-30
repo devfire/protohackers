0decbfabcbe9709a65ab64e6aeab286e6df09f85
Igor <igor@devfire.io>
2023-03-30 19:38:53 -0400

    more duplicate dispatch work

--- speed-daemon/src/handlers/dispatcher.rs
+++ speed-daemon/src/handlers/dispatcher.rs
@@ -12,19 +12,22 @@ pub async fn handle_i_am_dispatcher(
     tx: &mpsc::Sender<OutboundMessageType>,
     mut shared_db: Db,
 ) -> anyhow::Result<(), SpeedDaemonError> {
-    info!("Adding a dispatcher for roads {:?}", roads);
+    info!("From {client_addr} adding a dispatcher for roads {roads:?}");
 
     // make sure this client hasn't identified itself as a camera before
     if let Some(existing_camera) = shared_db.get_current_camera(client_addr).await {
         error!(
-            "{:?} sent a duplicate camera message {:?}",
-            client_addr, existing_camera
+            "{client_addr} previously identified as a camera: {:?}",
+            existing_camera
         );
         return Err(SpeedDaemonError::DuplicateCamera);
     }
 
     for road in roads.iter() {
-        if shared_db.ticket_dispatcher_already_exists(road, client_addr).await {
+        if shared_db
+            .ticket_dispatcher_already_exists(road, client_addr)
+            .await
+        {
             error!("Road {road} already has a dispatcher for {client_addr}");
             return Err(SpeedDaemonError::DuplicateDispatcher);
         } else {
