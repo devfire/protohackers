b4586707ebdb8fd3df88a72d795e5f7c4bb390f0
Igor <igor@devfire.io>
2023-03-30 12:05:02 -0400

    fixed up plate.rs

--- speed-daemon/src/handlers/plate.rs
+++ speed-daemon/src/handlers/plate.rs
@@ -15,12 +15,11 @@ pub async fn handle_plate(
     plate_tx: mpsc::Sender<PlateRoadStruct>,
     shared_db: Db,
 ) -> anyhow::Result<()> {
-    // Init an empty struct
-    let mut new_plate_road = PlateRoadStruct::new(String::from(""), 0);
-    let current_camera: InboundMessageType = InboundMessageType::default();
-
     // Get the camera that reported this plate
     if let Some(current_camera) = shared_db.get_current_camera(client_addr).await {
+        // Init an empty struct
+        let mut new_plate_road = PlateRoadStruct::new(String::from(""), 0);
+
         // deconstruct the IAmCamera message to get the plate+road combo
         if let InboundMessageType::IAmCamera {
             road,
@@ -33,22 +32,22 @@ pub async fn handle_plate(
                 road,
             };
         };
+
+        let new_ts_camera = TimestampCameraStruct {
+            timestamp: new_timestamp,
+            camera: current_camera,
+        };
+
+        info!("Adding {:?} {:?}", new_plate_road, new_ts_camera);
+
+        // add the newly observed plate:road combo to the shared db
+        shared_db
+            .add_plate_road_timestamp_camera(new_plate_road.clone(), new_ts_camera)
+            .await;
+
+        // send it off to the ticket_manager for processing
+        plate_tx.send(new_plate_road).await?;
     }
-
-    let new_ts_camera = TimestampCameraStruct {
-        timestamp: new_timestamp,
-        camera: current_camera,
-    };
-
-    info!("Adding {:?} {:?}", new_plate_road, new_ts_camera);
-
-    // add the newly observed plate:road combo to the shared db
-    shared_db
-        .add_plate_road_timestamp_camera(new_plate_road.clone(), new_ts_camera)
-        .await;
-
-    // send it off to the ticket_manager for processing
-    plate_tx.send(new_plate_road).await?;
 
     Ok(())
 }
