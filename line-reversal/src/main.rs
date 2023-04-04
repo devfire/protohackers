862809e37e2a369575f38b775df790f0a273bb9e
Igor <igor@devfire.io>
2023-04-04 18:13:21 -0400

    first listen scaffolding

--- line-reversal/src/main.rs
+++ line-reversal/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use env_logger::Env;
+use log::info;
+// use futures::{FutureExt, SinkExt};
+use tokio::net::UdpSocket;
+use tokio_util::udp::UdpFramed;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    // Setup the logging framework
+    let env = Env::default()
+        .filter_or("LOG_LEVEL", "info")
+        .write_style_or("LOG_STYLE", "always");
+
+    env_logger::init_from_env(env);
+    let socket = UdpSocket::bind("0.0.0.0:8080").await?;
+    info!("Listening on {}", socket.local_addr()?);
+
+    Ok(())
 }
